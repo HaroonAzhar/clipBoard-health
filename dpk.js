189d15c9f52ca33e6b73c7f771cb65ee6b5e88db
Haroon Azhar Khan <haroon1azhar@gmail.com>
2022-11-20 00:01:23 +0500

    refactored dpk code

--- dpk.js
+++ dpk.js
@@ -6,29 +6,20 @@ const TRIVIAL_PARTITION_KEY = "0";
 const MAX_PARTITION_KEY_LENGTH = 256;
 exports.deterministicPartitionKey = (event) => {
   let candidateKey;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidateKey = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidateKey = this.createHash(data);
-    }
-  }
+  if (!event) return TRIVIAL_PARTITION_KEY;
+  candidateKey = event.partitionKey ? event.partitionKey : this.createHash();
 
   return this.stringifiedReducedCandidateKey(candidateKey);
 };
 
 exports.createHash = (toUpdate) => {
-  return crypto.createHash("sha3-512").update(toUpdate).digest("hex");
+  return toUpdate
+    ? crypto.createHash("sha3-512").update(toUpdate).digest("hex")
+    : crypto.createHash("sha3-512").digest("hex");
 };
 exports.stringifiedReducedCandidateKey = (candidate) => {
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = this.createHash(candidate);
