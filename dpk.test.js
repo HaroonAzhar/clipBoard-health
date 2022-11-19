95c7ef950dadf5d504dd5547a12f09345594b505
Haroon Azhar Khan <haroon1azhar@gmail.com>
2022-11-19 22:24:20 +0500

    implemented test for falsy input to dpk function

--- dpk.test.js
+++ dpk.test.js
@@ -1,26 +1,27 @@
-const { deterministicPartitionKey } = require("./dpk");
+let library = require("./dpk");
 const { test1, test2, test3 } = require("./testFactory");
 const crypto = require("crypto");
-const jest = require("jest");
-
 describe("deterministicPartitionKey", () => {
-  jest.fn("Ha$h2!");
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
   it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
+    const trivialKey = library.deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
   it("Return value of 'partitionKey' when event object with 'partitionKey' passed", () => {
-    const trivialKey = deterministicPartitionKey(test1.data);
+    const trivialKey = library.deterministicPartitionKey(test1.data);
     expect(trivialKey).toBe("string ok values");
   });
   it("Returns value of 'partitionKey' stringified when event object number 'partitionKey' passed", () => {
-    const trivialKey = deterministicPartitionKey(test3.data);
+    const trivialKey = library.deterministicPartitionKey(test3.data);
     expect(trivialKey).toBe("3");
   });
-
   it("Returns hash when a falsy value for 'partitionKey'  is  passed", () => {
-    const trivialKey = deterministicPartitionKey(test4.data);
-
-    expect(trivialKey).toBe("3");
+    library.createHash = jest.fn().mockImplementation(() => {
+      return "9603ab3de34eb8675f94384b3d7d73";
+    });
+    const trivialKey = library.deterministicPartitionKey(test2.data);
+    expect(trivialKey).toEqual("9603ab3de34eb8675f94384b3d7d73");
   });
 });
