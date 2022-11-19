8c8297fa867aeee1bba9262cbb7ae258968f2928
Haroon Azhar Khan <haroon1azhar@gmail.com>
2022-11-19 23:48:35 +0500

    renamed processCandidateKey function

--- dpk.js
+++ dpk.js
@@ -1,29 +1,28 @@
 const crypto = require("crypto");
 const { test1, test2, test3 } = require("./testFactory");
 
-
 //can be  moved to some  global config files
 const TRIVIAL_PARTITION_KEY = "0";
 const MAX_PARTITION_KEY_LENGTH = 256;
 exports.deterministicPartitionKey = (event) => {
-  let candidate;
+  let candidateKey;
 
   if (event) {
     if (event.partitionKey) {
-      candidate = event.partitionKey;
+      candidateKey = event.partitionKey;
     } else {
       const data = JSON.stringify(event);
-      candidate = this.createHash(data);
+      candidateKey = this.createHash(data);
     }
   }
 
-  return this.processCandidateKey(candidate);
+  return this.stringifiedReducedCandidateKey(candidateKey);
 };
 
 exports.createHash = (toUpdate) => {
   return crypto.createHash("sha3-512").update(toUpdate).digest("hex");
 };
-exports.processCandidateKey = (candidate) => {
+exports.stringifiedReducedCandidateKey = (candidate) => {
   if (candidate) {
     if (typeof candidate !== "string") {
       candidate = JSON.stringify(candidate);
